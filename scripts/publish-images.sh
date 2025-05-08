d2776cbe814665d85092f7160682e77bd92b1fcd
Anton Ganhammar <ganhammar@gmail.com>
2025-05-08 09:52:06 +0200

    fix: set missing vars

--- scripts/publish-images.sh
+++ scripts/publish-images.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 docker_command=$DOCKER_COMMAND
+github_org=$DOCKER_USERNAME
+namespace_prefix='azure-container-apps-job-with-event-hub-integration'
+
 $docker_command login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD ghcr.io
 
 for image in processor; do
